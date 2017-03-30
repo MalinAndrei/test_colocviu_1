6b4a87b39bbf54f02d586126bcda8d1fc83e3ae7
Malin <malin@malin.pc>
2017-03-30 15:22:32 +0300

    after 2 tasks

--- PracticalTest012/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/practicaltest01/PracticalTest01MainActivity.java
+++ PracticalTest012/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/practicaltest01/PracticalTest01MainActivity.java
@@ -1,13 +1,52 @@
 package ro.pub.cs.systems.eim.practicaltest01.practicaltest01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class PracticalTest01MainActivity extends AppCompatActivity {
+public class PracticalTest01MainActivity extends Activity {
+
+    private EditText leftEditText = null;
+    private EditText rightEditText = null;
+    private Button leftButton = null;
+    private Button rightButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_main);
+        leftEditText = (EditText)findViewById(R.id.left_edit_text);
+        rightEditText = (EditText)findViewById(R.id.right_edit_text);
+        leftEditText.setText(String.valueOf(0));
+        rightEditText.setText(String.valueOf(0));
+
+        leftButton = (Button)findViewById(R.id.left_button);
+        rightButton = (Button)findViewById(R.id.right_button);
+    }
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.left_button:
+                    int leftNumberOfClicks = Integer.parseInt(leftEditText.getText().toString());
+                    leftNumberOfClicks++;
+                    leftEditText.setText(String.valueOf(leftNumberOfClicks));
+                    break;
+                case R.id.right_button:
+                    int rightNumberOfClicks = Integer.parseInt(rightEditText.getText().toString());
+                    rightNumberOfClicks++;
+                    rightEditText.setText(String.valueOf(rightNumberOfClicks));
+                    break;
+            }
+        }
     }
 }
